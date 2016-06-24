bb13251f8d5e224e90999361eb0f8cd6e646a406
Leonardo Rossetti <lrossetti@localhost.localdomain>
2016-06-24 08:52:20 -0300

    application.js rollback

--- server.js
+++
@@ -1,8 +0,0 @@
-var app = require('./application');
-
-var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8010;
-var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-var server = app.listen(port, host, function() {
-  console.log("App started at: " + new Date() + " on port: " + port);
-});
