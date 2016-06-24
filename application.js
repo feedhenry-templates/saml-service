d3364454df0421c4191957f60f945312b632b928
Leonardo Rossetti <lrossetti@localhost.localdomain>
2016-06-24 08:52:20 -0300

    Updated fh-mbaas-api dependency version

--- application.js
+++ application.js
@@ -46,8 +46,4 @@ app.use(mbaasExpress.fhmiddleware());
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
 
-var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8010;
-var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-var server = app.listen(port, host, function() {
-  console.log("App started at: " + new Date() + " on port: " + port);
-});
+module.exports = app;
