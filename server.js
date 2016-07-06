50970cc7a5527fe8a816ad3ec2a350c1446d0a1b
Phil Brookes <phil.brookes@redhat.com>
2016-07-06 11:12:13 +0100

    change port to 8001

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./application');
 
-var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8010;
+var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var server = app.listen(port, host, function() {
